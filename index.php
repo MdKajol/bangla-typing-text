ace51cf266caefdd041177a9c1babe997b358614
MdKajol <kajolahammed@gmail.com>
2018-11-27 11:22:32 +0600

    v1.0;

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="bn">
+<head>
+	<meta charset="UTF-8">
+	<title>Bangla Typing Speed</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="keyboard">
+					<input type="button" value="K">
+					<input type="button" value="L">
+					<input type="button" value="M">
+					<input type="button" value="N">
+					<input type="button" value="O">
+					<input type="button" value="P">
+					<input type="button" value="Q">
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+
+	<script src="assets/js/jquery-3.2.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
